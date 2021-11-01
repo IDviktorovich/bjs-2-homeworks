08a972d565b54d564ba6bbfac716e810fa41a363
Dima Ievlev <harajukuken1997@gmail.com>
2021-11-01 16:33:31 +0300

    Первое задание (без задания со звёздочкой)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    let x = (-b) / (2 * a);
+    arr = [x];
+  } else {
+    let x1, x2;
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+  }
   return arr; // array
 }
 
