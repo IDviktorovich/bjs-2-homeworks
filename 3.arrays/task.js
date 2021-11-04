cc6a5dbc401773ae3bf44c85c319ea412c0bfdee
Dima Ievlev <harajukuken1997@gmail.com>
2021-11-04 15:59:10 +0300

    Добавлены 2 задания по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = (arr1.length === arr2.length) && arr1.every((item, idx) => item === arr2[idx]); 
+  
   return result; // boolean
 }
 
+let comparison = compareArrays([5,6,7], [5,6,7]);
+console.log(comparison);
+
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
+
+console.log(advancedFilter([-1,-2]));
