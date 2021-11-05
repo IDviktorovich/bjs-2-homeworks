5f3143cd6b7ce06306eb855e3584f6c6fd31c9a7
Dima Ievlev <harajukuken1997@gmail.com>
2021-11-05 17:11:21 +0300

    Решенная задача

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,78 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  let subject;
+  this.subject = subjectName;
+  return {
+    subject,
+  };
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  let marks;
+  
+  if (this.marks === undefined) { 
+    this.marks = Array.from(mark);
+    this.marks.unshift(mark);
+  } else {
+      this.marks.push(mark);
+    }
+  
+  return {
+    marks
+    };
+}
+
+Student.prototype.addMarks = function (...markN) {
+  let marks;
+  this.marks = Array.from(markN)
+  return {
+    marks,
+  };
+}
+
+Student.prototype.getAverage = function () {
+  let avg;
+  avg = this.marks.reduce((acc, item, idx, arr) => {
+    acc += item;
+    if (idx === arr.length - 1) {
+      return acc / arr.length;
+    } else {
+      return acc;
+    }
+  },)
+
+  return {
+    avg,
+  };
+}
+
+Student.prototype.exclude = function (reason) {  
+  delete this.subject;
+  delete this.marks;
+
+  let excluded;
+  this.excluded = reason;
+  
+  return {
+    excluded,
+  };
+}
+
+const dima = new Student("Dima", "male", "24");
+const tanya = new Student("Tanya", "female", "23");
+
+dima.addMark(5);
+dima.addMark(4);
+dima.addMark(3);
+console.log(dima.getAverage());
+console.log(dima);
+tanya.addMarks(10, 2, 4, 8);
+tanya.setSubject("Math11");
+tanya.exclude("low grades");
+console.log(tanya);
+
